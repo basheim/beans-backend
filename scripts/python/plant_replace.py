8fe4ac080d1be852620272ec72804fea82a2d957
Brandon Asheim <brandonasheim@gmail.com>
2022-09-02 12:12:49 -0700

    feat(): attempting new configs for connections

--- scripts/python/plant_replace.py
+++ scripts/python/plant_replace.py
@@ -3,10 +3,24 @@
 import sys
 import getopt
 import csv
+import requests
+import os
 
+base_url = "http://beans-backend-lb-282639646.us-west-2.elb.amazonaws.com:80"
+post_delete_url = "/api/v1/plants"
+to_delete = False
+api_user = "admin"
+api_password = os.getenv('API_PASSWORD')
 
 def main(argv):
+    # get the file arg
     input_file = get_input_file(argv)
+    # delete all data
+    session = requests.session()
+    session.auth = (api_user, api_password)
+    if to_delete:
+        session.delete(url=base_url + post_delete_url)
+
     with open(input_file, newline='') as csv_file:
         reader = csv.DictReader(csv_file)
         for row in reader:
@@ -16,20 +30,21 @@ def main(argv):
 def get_input_file(argv):
     input_file = None
     try:
-        opts, args = getopt.getopt(argv,"hi:")
+        opts, args = getopt.getopt(argv,"hdi:")
     except getopt.GetoptError:
-        print('plant_replace.py -i <inputfile>')
+        print('plant_replace.py -i opt:<input file> -d <delete all data>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('plant_replace.py -i <inputfile>')
+            print('plant_replace.py -i opt:<input file> -d <delete all data>')
             sys.exit()
         elif opt == "-i":
             input_file = arg
+        elif opt == "-d":
+            to_delete = True
     if not input_file:
         print('-i is required')
         sys.exit()
-    print('Input file is "', input_file)
     return input_file
 
 
