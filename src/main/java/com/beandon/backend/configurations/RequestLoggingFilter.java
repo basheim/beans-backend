83a598f61094dc9ab2b00db55c34ce353bd52636
Brandon Asheim <brandonasheim@gmail.com>
2022-09-09 13:49:08 -0700

    fix(): resolve multi request logging issue

--- src/main/java/com/beandon/backend/configurations/RequestLoggingFilter.java
+++ src/main/java/com/beandon/backend/configurations/RequestLoggingFilter.java
@@ -1,12 +1,10 @@
 package com.beandon.backend.configurations;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Set;
 
-@Component
 public class RequestLoggingFilter extends AbstractRequestLoggingFilter {
 
     private final Set<String> excludedUrls = Set.of("/health");
@@ -21,11 +19,11 @@ public class RequestLoggingFilter extends AbstractRequestLoggingFilter {
 
     @Override
     protected void beforeRequest(HttpServletRequest request, String message) {
-        logger.info(message);
+        this.logger.info(message);
     }
 
     @Override
     protected void afterRequest(HttpServletRequest request, String message) {
-        logger.info(message);
+        this.logger.info(message);
     }
 }
--- src/main/java/com/beandon/backend/configurations/RequestLoggingFilterConfig.java
+++ src/main/java/com/beandon/backend/configurations/RequestLoggingFilterConfig.java
@@ -3,19 +3,22 @@ package com.beandon.backend.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.AbstractRequestLoggingFilter;
-import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
 public class RequestLoggingFilterConfig {
 
     @Bean
-    public AbstractRequestLoggingFilter logFilter() {
+    public AbstractRequestLoggingFilter requestLoggingFilter() {
         AbstractRequestLoggingFilter filter
                 = new RequestLoggingFilter();
         filter.setIncludeQueryString(true);
         filter.setIncludePayload(true);
         filter.setMaxPayloadLength(10000);
         filter.setIncludeHeaders(false);
+        filter.setBeforeMessagePrefix("Attempting Request: ");
+        filter.setBeforeMessageSuffix("");
+        filter.setAfterMessagePrefix("Request Completed: ");
+        filter.setAfterMessageSuffix("");
         return filter;
     }
 }
