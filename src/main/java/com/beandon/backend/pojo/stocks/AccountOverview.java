1b2e29de6d057e155c13c2561311216ef12ca314
Brandon Asheim <brandonasheim@gmail.com>
2023-01-24 19:51:51 -0800

    fix(): update lombok types

--- src/main/java/com/beandon/backend/pojo/stocks/AccountOverview.java
+++ src/main/java/com/beandon/backend/pojo/stocks/AccountOverview.java
@@ -1,8 +1,12 @@
 package com.beandon.backend.pojo.stocks;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
+import lombok.Data;
 
 @Builder
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AccountOverview {
     private int amount;
     private double percentChange;
