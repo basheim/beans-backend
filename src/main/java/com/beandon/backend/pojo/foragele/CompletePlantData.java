a19a018ebff35243f6f8ed4ad6bac76445cd0dd7
Brandon Asheim <brandonasheim@gmail.com>
2023-03-10 10:42:16 -0800

    fix(): add back nonnulls for data object

--- src/main/java/com/beandon/backend/pojo/foragele/CompletePlantData.java
+++ src/main/java/com/beandon/backend/pojo/foragele/CompletePlantData.java
@@ -1,20 +1,33 @@
 package com.beandon.backend.pojo.foragele;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
 
 import java.sql.Timestamp;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class CompletePlantData {
+    @NonNull
     private String id;
+    @NonNull
     private String poisonousLookAlike;
+    @NonNull
     private String edibility;
+    @NonNull
     private String foundNear;
+    @NonNull
     private String keyFeatures;
+    @NonNull
     private String imageUrl;
+    @NonNull
     private String english;
+    @NonNull
     private String latin;
     private Timestamp start;
     private Timestamp end;
